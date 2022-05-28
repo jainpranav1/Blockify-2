9877d428b20673eea0ec109180bcd20ef02023cc
Pranav Jain <pranavji@tamu.edu>
2022-05-28 11:22:31 -0500

    added passport

--- server.js
+++ server.js
@@ -39,31 +39,32 @@ mongoose.connect(process.env.DBURI, {
 
 // handle get requests
 
-app.get('/', (req, res) => {
-    res.render('home')
+app.get('/', get_auth, (req, res) => {
+
+    res.render('home', {is_auth: res.locals.is_auth})
 })
 
-app.get('/dashboard', checkAuthenticated, (req, res) => {
-    res.render('dashboard')
+app.get('/dashboard', redirect_not_auth, (req, res) => {
+    res.render('dashboard', {is_auth: true})
 })
 
-app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('login')
+app.get('/login', redirect_auth, (req, res) => {
+    res.render('login', {is_auth: false})
 })
 
-app.get('/sign_up', checkNotAuthenticated, (req, res) => {
-    res.render('sign_up')
+app.get('/sign_up', redirect_auth, (req, res) => {
+    res.render('sign_up', {is_auth: false})
 })
 
 // handle post requests
 
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+app.post('/login', passport.authenticate('local', {
     successRedirect: '/dashboard',
     failureRedirect: '/login',
     failureFlash: true
 }))
 
-app.post('/sign_up', checkNotAuthenticated, async (req, res) => {
+app.post('/sign_up', async (req, res) => {
 
     const salt = await bcrypt.genSalt()
     const hashedPassword = await bcrypt.hash(req.body.password, salt)
@@ -85,20 +86,33 @@ app.delete('/logout', async (req, res) => {
     });
 })
 
-function checkAuthenticated(req, res, next) {
+// add is_auth attribute to res.locals
+function get_auth(req, res, next) {
+    if (req.isAuthenticated()) {
+        res.locals.is_auth = true
+        return next()
+    }
+
+    res.locals.is_auth = false
+    return next()
+}
+
+// redirect to dashboard if authenticated
+function redirect_auth(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/dashboard', {is_auth: true})
+    }
+
+    return next()
+}
+
+// redirect to login if not authenticated
+function redirect_not_auth(req, res, next) {
     if (req.isAuthenticated()) {
         return next()
     }
 
-    res.redirect('/login') 
-}
-
-function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return res.redirect('/dashboard')
-    }
-
-    next()
+    return res.redirect('/login', {is_auth: false})
 }
 
 app.listen(5000)
